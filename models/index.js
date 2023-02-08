e974a71283d670eb319766f2579077da4967a279
Greg Trahan <trahan.greg@gmail.com>
2023-02-07 22:57:28 -0600

    add models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,35 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "reader_id",
+});
 
 // Categories have many Products
+Category.hasOne(Product, {
+  // foreignKey: 'reader_id',
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Category, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
